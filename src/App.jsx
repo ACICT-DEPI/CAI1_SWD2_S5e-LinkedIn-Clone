e543eef6faa1ddc968643668a810e99be1d46255
Amera <amera@gmail.com>
2024-09-07 23:28:44 +0300

    Route installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
   
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <RouterProvider router={router} />
   )
 }
 
