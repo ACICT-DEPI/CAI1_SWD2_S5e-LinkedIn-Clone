76b9648b5b1e969521c13d8437d306cd2adca637
Amera <amera@gmail.com>
2024-09-08 19:04:18 +0300

    signin&signup pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 
 import './App.css'
+import WelcomePage from './pages/WelcomePage';
+import SignUpPage from './pages/SignUpPage';
+import SignUpDetailsPage from './pages/SignUpDetailsPage';
+import LoginPage from './pages/LoginPage';
+import SignUpLayout from './layouts/SignUpLayout';
+
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 
+
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="sign-up" element={<SignUpLayout />}>
+          <Route index element={<SignUpPage />} /> 
+          <Route path="details" element={<SignUpDetailsPage />} />
+        </Route>
+        <Route path="login" element={<LoginPage />} />
+    </>
+    )
+  );
   
   return (
     <RouterProvider router={router} />
