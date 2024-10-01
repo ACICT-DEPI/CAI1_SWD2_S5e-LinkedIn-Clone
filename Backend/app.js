e8eb3b60d418941ffc107499685fa8f68b7f4543
Meefr <mohamedelramah737@gmail.com>
2024-10-01 12:04:36 +0300

    merging with main

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./db/connectDB");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+
+// Init App
+const app = express();
+
+// Apply Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port", PORT);
+});
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+  getSuggstedConnections,
+  getPublicProfile,
+  UpdateProfile,
+} = require("../controllers/user.controller.js");
+const { protectRoute } = require("../controllers/auth.controller.js");
+const router = express.Router();
+
+//get
+router.get("/suggestions", protectRoute, getSuggstedConnections);
+router.get("/:username", protectRoute, getPublicProfile);
+
+//put
+router.put("/profile", protectRoute, UpdateProfile);
+
+module.exports = router;
