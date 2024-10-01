bf7e67fbb87932be5a3d0f22817cc77cfdb00e4b
haneen akram <nonakram@gmail.com>
2024-10-01 16:30:03 +0200

    updates in user functions

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,8 +1,9 @@
 const { User } = require("../models/user.model.js");
 
- const getSuggstedConnections = async (req, res) => {
+const getSuggstedConnections = async (req, res) => {
   try {
-    //dont recommend my rofile nor the users i have
+    //select the profile, name , header, path to their profile
+    //dont recommend my profile($ne) nor the users i have ($nin)
     const curentUser = await User.findById(req.user._id).select("connections");
     const suggestedUser = await User.find({
       _id: { $ne: req.user._id, $nin: curentUser.connections },
@@ -10,15 +11,15 @@ const { User } = require("../models/user.model.js");
       .select("name username profilePicture headline")
       .limit(3);
     res.json(suggestedUser);
-    //profile, name , header, path to their profile
   } catch (error) {
     console.log("error in  getSuggstedConnections:", error);
     res.status(500).json({ message: "server error" });
   }
 };
- const getPublicProfile = async (req, res) => {
+const getPublicProfile = async (req, res) => {
   try {
-    const user = await User.findOne({ usrname: req.params.usrname }).select(
+    console.log("Received request for username:", req.params.username);
+    const user = await User.findOne({ username: req.params.username }).select(
       "-password"
     );
     if (!user) {
@@ -30,7 +31,8 @@ const { User } = require("../models/user.model.js");
     res.status(500).json({ message: "server error" });
   }
 };
- const UpdateProfile = async (req, res) => {
+
+const UpdateProfile = async (req, res) => {
   try {
     const allowedField = [
       "firstName",
@@ -53,10 +55,10 @@ const { User } = require("../models/user.model.js");
     }
     //todo: profile and banner img
     const user = await User.findByIdAndUpdate(
-      req.user._id,
+      req.body.id,
       { $set: updatedData },
       { new: true }
-    ).select("-password"); // the new returns the pbject after the update is done
+    ).select("-password"); // the new returns the user object after the update is done
     res.json(user);
   } catch (error) {
     console.log("error in  Up dateProfile:", error);
@@ -64,8 +66,33 @@ const { User } = require("../models/user.model.js");
   }
 };
 
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log("error in  Up getAllUsers:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// const getUserById = async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id); // Use req.params.id instead of req.body.id
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+//     res.status(200).json(user);
+//   } catch (error) {
+//     console.log("error in  Up getById:", error);
+//     res.status(500).json({ message: "server error" });
+//   }
+// };
+
 module.exports = {
   getSuggstedConnections,
   getPublicProfile,
   UpdateProfile,
+  // getUserById,
+  getAllUsers,
 };
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -3,15 +3,19 @@ const {
   getSuggstedConnections,
   getPublicProfile,
   UpdateProfile,
+  getAllUsers,
+  getUserById,
 } = require("../controllers/user.controller.js");
 const { protectRoute } = require("../controllers/auth.controller.js");
 const router = express.Router();
 
 //get
-router.get("/suggestions", protectRoute, getSuggstedConnections);
-router.get("/:username", protectRoute, getPublicProfile);
+router.get("/", protectRoute, getAllUsers); //ok
+router.get("/suggestions", protectRoute, getSuggstedConnections); //ok
+router.get("/:username", protectRoute, getPublicProfile); //ok //example api call: http://localhost:5000/api/users/haneen
+// router.get("/:id", getUserById);
 
 //put
-router.put("/profile", protectRoute, UpdateProfile);
+router.put("/profile", UpdateProfile); //ok
 
 module.exports = router;
