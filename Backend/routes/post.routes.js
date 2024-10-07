2b7471fcbbebae269fb4de6362298d1a40f61297
Meefr <mohamedelramah737@gmail.com>
2024-10-08 00:03:59 +0300

    adding verification section

--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const postController = require('../controllers/post.controller.js')
+const express = require("express");
+const postController = require("../controllers/post.controller.js");
+const { verifyToken } = require("../middleware/verifyToken.js");
+const { protectRoute } = require("../controllers/auth.controller.js");
 const router = express.Router();
 
-router.get("/admin", postController.getAllPosts);
-router.get("/", postController.getFeedPosts);
-router.patch("/share", postController.sharePost); // takes userId who repost and postId that is been reposted
-router.get('/comments/:id',postController.getAllComments)
-router.patch("/", postController.createPost);
-router.get("/:id", postController.getPostById);
-router.delete("/:id", postController.deletePost);
+router.get("/admin", verifyToken, postController.getAllPosts);
+router.get("/", verifyToken, postController.getFeedPosts);
+router.patch("/share", verifyToken, postController.sharePost); // takes userId who repost and postId that is been reposted
+router.get("/comments/:id", verifyToken, postController.getAllComments);
+router.patch("/", verifyToken, postController.createPost);
+router.get("/:id", verifyToken, postController.getPostById);
+router.delete("/:id", verifyToken, postController.deletePost);
 
 module.exports = router;
