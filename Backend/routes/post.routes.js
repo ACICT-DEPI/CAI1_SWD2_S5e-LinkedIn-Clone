886e89e35293dabecf2525ea6b15b435fec91104
Amera <amera@gmail.com>
2024-10-10 01:54:29 +0300

    edit verifyAndProtect on routes

--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const postController = require("../controllers/post.controller.js");
-const { verifyToken } = require("../middleware/verifyToken.js");
-const { protectRoute } = require("../controllers/auth.controller.js");
+// const { verifyToken } = require("../middleware/verifyToken.js");
+// const { protectRoute } = require("../controllers/auth.controller.js");
+const { verifyAndProtect } = require("../controllers/auth.controller.js");
 const router = express.Router();
 
-router.get("/admin", verifyToken, postController.getAllPosts);
-router.get("/", verifyToken, postController.getFeedPosts);
-router.patch("/share", verifyToken, postController.sharePost); // takes userId who repost and postId that is been reposted
-router.get("/comments/:id", verifyToken, postController.getAllComments);
-router.patch("/", verifyToken, postController.createPost);
-router.get("/:id", verifyToken, postController.getPostById);
-router.delete("/:id", verifyToken, postController.deletePost);
+router.get("/admin", verifyAndProtect, postController.getAllPosts);
+router.get("/", verifyAndProtect, postController.getFeedPosts);
+router.patch("/share", verifyAndProtect, postController.sharePost); // takes userId who repost and postId that is been reposted
+router.get("/comments/:id", verifyAndProtect, postController.getAllComments);
+router.patch("/", verifyAndProtect, postController.createPost);
+router.get("/:id", verifyAndProtect, postController.getPostById);
+router.delete("/:id", verifyAndProtect, postController.deletePost);
 
 module.exports = router;
