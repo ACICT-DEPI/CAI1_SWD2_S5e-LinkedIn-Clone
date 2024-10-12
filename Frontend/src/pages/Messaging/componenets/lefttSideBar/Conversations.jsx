7d65089413955522184b49cedd4ec2276bff3b10
Amera <amera@gmail.com>
2024-10-12 13:33:56 +0300

    improve chat

--- Frontend/src/pages/Messaging/componenets/lefttSideBar/Conversations.jsx
+++ Frontend/src/pages/Messaging/componenets/lefttSideBar/Conversations.jsx
@@ -1,18 +1,21 @@
 import useGetConversations from '../../../../hooks/useGetConversations';
 import Conversation from './Conversation';
 
-
 const Conversations = () => {
   const { loading, conversations } = useGetConversations();
+
   return (
     <div>
       {conversations.map((conv, index) => (
-        <Conversation key={conv._id} conversation={conv} />
+        <div key={conv._id}>
+          <Conversation conversation={conv} />
+          {/* Horizontal Separator */}
+          <hr className="border-t border-gray-300 my-1" />
+        </div>
       ))}
-
-      {/* {loading ? <span className='loading loading-spinner mx-auto'></span> : null} */}
+      {/* Optional loading spinner */}
+      {loading && <span className='loading loading-spinner mx-auto'></span>}
     </div>
-    
   );
 };
 
