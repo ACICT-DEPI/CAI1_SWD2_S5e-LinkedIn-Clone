d02edce0815a88f2247d62ec957ca857e0431150
Amera <amera@gmail.com>
2024-10-12 22:05:43 +0300

    edir search username

--- Frontend/src/pages/Messaging/componenets/lefttSideBar/Conversations.jsx
+++ Frontend/src/pages/Messaging/componenets/lefttSideBar/Conversations.jsx
@@ -1,20 +1,23 @@
 import useGetConversations from '../../../../hooks/useGetConversations';
 import Conversation from './Conversation';
 
-const Conversations = () => {
+const Conversations = ({ searchTerm }) => {
   const { loading, conversations } = useGetConversations();
+
+  // Filter conversations based on searchTerm
+  const filteredConversations = conversations.filter((conv) =>
+    conv.username.toLowerCase().includes(searchTerm) // Assuming 'name' is a field in each conversation
+  );
 
   return (
     <div>
-      {conversations.map((conv, index) => (
+      {filteredConversations.map((conv) => (
         <div key={conv._id}>
           <Conversation conversation={conv} />
-          {/* Horizontal Separator */}
           <hr className="border-t border-gray-300 my-1" />
         </div>
       ))}
-      {/* Optional loading spinner */}
-      {loading && <span className='loading loading-spinner mx-auto'></span>}
+      {loading && <span className="loading loading-spinner mx-auto"></span>}
     </div>
   );
 };
