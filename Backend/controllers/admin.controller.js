699e9a90d902b4b2afea29db48334f9164f0d23b
Meefr <mohamedelramah737@gmail.com>
2024-10-12 19:05:04 +0300

    adding almost all SSR functionalities

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -2,6 +2,7 @@ const Comments = require("../models/comments.model");
 const Connections = require("../models/connection.model");
 const Posts = require("../models/post.model");
 const { User } = require("../models/user.model");
+
 const renderUsersView = async (req, res) => {
   try {
     // Get page and limit from query parameters, default to page 1 and showing all users if not specified
@@ -29,7 +30,9 @@ const renderUsersView = async (req, res) => {
       totalUsers,
     });
   } catch (error) {
-    res.status(500).render("error.pug");
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
   }
 };
 const renderUserViewById = async (req, res) => {
@@ -40,101 +43,162 @@ const renderUserViewById = async (req, res) => {
     }
     res.status(200).render("users/userDetails", { user });
   } catch (error) {
-    res.status(500).render("error.pug", { message: "An error occurred" });
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
   }
 };
 
 ///users/:userId/posts
 const renderAllUserPosts = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query; // Default to page 1, 10 posts per page
-  const posts = await Posts.find({ auther: req.params.userId })
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .populate("comments") // Optionally populate comments
-    .exec();
-  const count = await Posts.countDocuments({ auther: req.params.userId });
-  const user = await User.findById(req.params.userId);
-  res.render("users/userPosts", {
-    user: user,
-    posts,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page,
-  });
+  try {
+    const { page = 1, limit = 10 } = req.query; // Default to page 1, 10 posts per page
+    const posts = await Posts.find({ auther: req.params.userId })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate("comments") // Optionally populate comments
+      .exec();
+    const count = await Posts.countDocuments({ auther: req.params.userId });
+    const user = await User.findById(req.params.userId);
+    res.render("users/userPosts", {
+      user: user,
+      posts,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
+  }
 };
 ///users/:userId/comments
 const renderAllUserComments = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const comments = await Comments.find({ userId: req.params.userId })
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .populate("postId") // Optionally populate post information
-    .exec();
-  const count = await Comments.countDocuments({
-    userId: req.params.userId,
-  });
-  const user = await User.findById(req.params.userId);
-  res.render("users/userComments", {
-    user: user,
-    comments,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page,
-  });
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const comments = await Comments.find({ userId: req.params.userId })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate("postId") // Optionally populate post information
+      .exec();
+    const count = await Comments.countDocuments({
+      userId: req.params.userId,
+    });
+    const user = await User.findById(req.params.userId);
+    res.render("users/userComments", {
+      user: user,
+      comments,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
+  }
 };
 
 ///users/:userId/connections
 const renderAllUserConnections = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const connections = await Connections.find({
-    $or: [{ senderId: req.params.userId }, { receiverId: req.params.userId }],
-  })
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .populate("senderId receiverId") // Populate both user details
-    .exec();
-  const count = await Connections.countDocuments({
-    $or: [{ senderId: req.params.userId }, { receiverId: req.params.userId }],
-  });
-  const user = await User.findById(req.params.userId);
-  res.render("users/userConnections", {
-    user: user,
-    connections,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page,
-  });
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const connections = await Connections.find({
+      $or: [{ senderId: req.params.userId }, { receiverId: req.params.userId }],
+    })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate("senderId receiverId") // Populate both user details
+      .exec();
+    const count = await Connections.countDocuments({
+      $or: [{ senderId: req.params.userId }, { receiverId: req.params.userId }],
+    });
+    const user = await User.findById(req.params.userId);
+    res.render("users/userConnections", {
+      user: user,
+      connections,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
+  } catch (error) {
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
+  }
 };
 
 //posts
 const renderAllPosts = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const posts = await Posts.find()
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .populate("auther comments") // Optionally populate user and comments
-    .exec();
-  const count = await Posts.countDocuments();
-  res.json({
-    posts,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page,
-  });
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const posts = await Posts.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate("auther comments") // Optionally populate user and comments
+      .sort({ createdAt: -1 })
+      .exec();
+    const count = await Posts.countDocuments();
+    res.render("posts", {
+      posts,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      limit: limit,
+    });
+  } catch (error) {
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
+  }
 };
 
 //comments
 
 const renderAllComments = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const comments = await Comments.find()
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .populate("userId postId") // Optionally populate user and post info
-    .exec();
-  const count = await Comments.countDocuments();
-  res.json({
-    comments,
-    totalPages: Math.ceil(count / limit),
-    currentPage: page,
-  });
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const comments = await Comments.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .populate("userId postId") // Optionally populate user and post info
+      .sort({ createdAt: -1 })
+      .exec();
+    const count = await Comments.countDocuments();
+    res.render("comments", {
+      comments,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      limit: limit,
+    });
+  } catch (error) {
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
+  }
 };
+
+const renderCommentById = async (req, res) => {
+  const commentId = req.params.id;
+  try {
+    const comment = await Comments.findById(commentId)
+      .populate("userId") // Populates the author of the comment
+      .populate({
+        path: "replies", // Populate the replies array
+        populate: { path: "userId" }, // Populate user info inside each reply
+      })
+      .exec();
+
+    console.log(comment);
+
+    res.render("comments/commentDetails.pug", {
+      comment,
+    });
+  } catch (error) {
+    console.error("Error fetching comment:", error);
+    res.status(500).render("error", {
+      back_url: "/api/admin/users",
+    });
+  }
+};
+
 module.exports = {
   renderUsersView,
   renderUserViewById,
@@ -143,4 +207,5 @@ module.exports = {
   renderAllUserConnections,
   renderAllPosts,
   renderAllComments,
+  renderCommentById,
 };
--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin.controller");
+
 router.get("/users", adminController.renderUsersView);
 router.get("/users/:userId", adminController.renderUserViewById);
 router.get("/users/:userId/posts", adminController.renderAllUserPosts);
@@ -10,4 +11,14 @@ router.get(
   adminController.renderAllUserConnections
 );
 
+router.get("/posts",adminController.renderAllPosts);
+
+router.get("/comments",adminController.renderAllComments);
+router.get("/comments/:id",adminController.renderCommentById);
+
+router.get("*",(req,res)=>{
+  res.render("error",{
+    back_url:"/api/admin/users"
+  })
+})
 module.exports = router;
