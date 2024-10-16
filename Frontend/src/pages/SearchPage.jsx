6247fff0cc1633f2e076d6108ae1787770277143
haneen akram <nonakram@gmail.com>
2024-10-16 04:01:50 +0200

    done with connections

--- Frontend/src/pages/SearchPage.jsx
+++ Frontend/src/pages/SearchPage.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Section from "../components/common/Section";
+import Button from "../components/common/Button";
+import { TiDelete } from "react-icons/ti";
+import { IoPersonAdd, IoHourglassOutline } from "react-icons/io5";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import userIcon from "../assets/images/nav/user.svg";
 import { Link } from "react-router-dom";
+
 const SearchPage = () => {
   const location = useLocation();
   const searchTerm = location.state?.searchTerm || "";
@@ -15,31 +19,31 @@ const SearchPage = () => {
       const page = 1;
       const limit = 10;
       try {
-          const response = await axios.get(`http://localhost:5000/api/users`, {
-            params: {
-              search: searchTerm,
-              page,
-              limit,
-            },
-          });
+        const response = await axios.get(`http://localhost:5000/api/users`, {
+          params: {
+            search: searchTerm,
+            page,
+            limit,
+          },
+        });
 
-          const { users } = response.data;
-          console.log(response.data);
+        const { users } = response.data;
+        console.log(response.data);
 
-          setSuggestedUsers(users);
-          const initialStatus = users.reduce((acc, user) => {
-            console.log(user.connectionStatus);
-
-            acc[user._id] = user.connectionStatus;
-            return acc;
-          }, {});
-          setConnectionStatus(initialStatus);
+        setSuggestedUsers(users);
+        const initialStatus = users.reduce((acc, user) => {
+          console.log(user.connectionStatus);
+          acc[user._id] = user.connectionStatus;
+          return acc;
+        }, {});
+        setConnectionStatus(initialStatus);
       } catch (error) {
         console.error("Error fetching search results", error);
       }
     };
     fetchData();
   }, [searchTerm]);
+
   const sendConnectionRequest = async (userId) => {
     try {
       const currentStatus = connectionStatus[userId];
@@ -55,6 +59,7 @@ const SearchPage = () => {
         );
         console.log(" request :", response);
         console.log("Connection request sent:", response.data);
+
         // Update the connection status to "pending"
         setConnectionStatus((prevStatus) => ({
           ...prevStatus,
@@ -79,6 +84,7 @@ const SearchPage = () => {
       console.error("Error sending connection request:", error);
     }
   };
+
   return (
     <div className="mt-20">
       <Section>
@@ -104,17 +110,31 @@ const SearchPage = () => {
                   <div className="text-gray-500">{user.headline}</div>
                 </div>
 
-                <button
-                  className={`bg-transparent text-black/60 shadow-inner border border-black/60 py-4 px-4 rounded-full flex items-center 
-              font-semibold justify-center max-h-8 max-w-[480px] text-center outline-none 
-              hover:border-black hover:border-1 ${
-                connectionStatus[user._id] === "accepted" ? "hidden" : "block"
-              }
-              ${connectionStatus[user._id] === "pending" ? "bg-gray-500" : ""}`}
-                  onClick={() => sendConnectionRequest(user._id)}
-                >
-                  {connectionStatus[user._id]}
-                </button>
+                <Button
+                  label={
+                    connectionStatus[user._id] === "pending"
+                      ? "Pending"
+                      : "Connect"
+                  }
+                  icon={
+                    connectionStatus[user._id] === "pending" ? (
+                      <IoHourglassOutline />
+                    ) : (
+                      <IoPersonAdd />
+                    )
+                  }
+                  styleType={
+                    connectionStatus[user._id] === "pending"
+                      ? "default"
+                      : "outline"
+                  }
+                  className={`w-1/4 mx-auto my-5 py-1 ${
+                    connectionStatus[user._id] === "accepted"
+                      ? "hidden"
+                      : "block"
+                  }`}
+                  onClick={() => sendConnectionRequest(user._id)} // Pass user._id here
+                />
               </div>
             </li>
           ))}
